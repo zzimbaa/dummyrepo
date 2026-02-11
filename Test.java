e90296147bebafe9a4513b2a70362b2a9af091f3
Albin Blomqvist <albinbl@kth.se>
2026-02-12 00:36:20 +0100

    main

---
+++ Test.java
@@ -0,0 +1,27 @@
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test class
+ */
+public class Test
+{
+
+   /**
+     * Tests that at least one test fails
+     */
+    @Test
+    public void simpleTest() {
+        int sum = 1+1;
+        assertTrue(sum==2);
+    }
+
+
+
+
+}
